22541a1578f907d544eee630a6f8c1f5670dc2c9
thomas <thomasdany38@gmail.com>
2026-08-31 14:16:12 +0700

    add feature: Cek Stok, dan enhance payment feature

--- main.py
+++ main.py
@@ -6,10 +6,27 @@ price_apple = 10_000
 price_orange = 15_000
 price_grape = 20_000
 
+# Stock Buah
+stock_apple = 10
+stock_orange = 15
+stock_grape = 20
+
 # Menu Input QTY
 qty_apple = int(input("Masukan Jumlah Apel: "))
+while qty_apple > stock_apple:
+    print(f"Jumlah yang dimasukkan terlalu banyak !")
+    print(f"Stock Apel Tinggal: {stock_apple}")
+    qty_apple = int(input("Masukan Jumlah Apel: "))
 qty_orange = int(input("Masukan Jumlah Jeruk: ")) 
+while qty_orange > stock_orange:
+    print(f"Jumlah yang dimasukkan terlalu banyak !")
+    print(f"Stock Jeruk Tinggal: {stock_orange}")
+    qty_orange = int(input("Masukan Jumlah Jeruk: "))
 qty_grape = int(input("Masukan Jumla Anggur: "))
+while qty_grape > stock_grape:
+    print(f"Jumlah yang dimasukkan terlalu banyak !")
+    print(f"Stock Anggur Tinggal: {stock_grape}")
+    qty_grape = int(input("Masukan Jumlah Anggur: "))
 
 #Perhitungan
 total_apple = qty_apple*price_apple
@@ -26,12 +43,14 @@ print (f"\nTotal  : Rp.{grand_total:,}\n")
 
 #Payment Feature 
 print ("-"*10)
+
 payment = int(input("Masukan Jumlah Uang: "))
 selisih = payment - grand_total
-
-if(selisih < 0):
+while selisih < 0:
     print("\n[X] Tansaksi dibatalkan !")
     print(f"Uang kurang sebesar Rp.{abs(selisih):,}")
+    payment = int(input("\nMasukan Jumlah Uang: "))
+    selisih = payment - grand_total
 else:
     print("\nTerima Kasih !")
     if (selisih):
