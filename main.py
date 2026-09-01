51e990ec6f51e4023ceb6208136366cccacebd4b
thomas <thomasdany38@gmail.com>
2026-09-01 13:30:47 +0700

    modify data list

--- main.py
+++ main.py
@@ -1,45 +1,44 @@
 import os
 os.system('cls')
 
-# Harga Buah
-price_apple = 10_000
-price_orange = 15_000
-price_grape = 20_000
-
-# Stock Buah
-stock_apple = 10
-stock_orange = 15
-stock_grape = 20
+#Data Buah
+products = [
+    #name, price, stock
+    ["Apel", 10_000, 10], 
+    ["Jeruk", 15_000, 10], 
+    ["Anggur", 20_000, 10]
+]
 
 # Menu Input QTY
 qty_apple = int(input("Masukan Jumlah Apel: "))
-while qty_apple > stock_apple:
+while qty_apple >products[0[2]]:
     print(f"Jumlah yang dimasukkan terlalu banyak !")
-    print(f"Stock Apel Tinggal: {stock_apple}")
+    print(f"Stock Apel Tinggal: {products[0][2]}")
     qty_apple = int(input("Masukan Jumlah Apel: "))
 qty_orange = int(input("Masukan Jumlah Jeruk: ")) 
-while qty_orange > stock_orange:
+while qty_orange > products[1][2]:
     print(f"Jumlah yang dimasukkan terlalu banyak !")
-    print(f"Stock Jeruk Tinggal: {stock_orange}")
+    print(f"Stock Jeruk Tinggal: {products[1][2]}")
     qty_orange = int(input("Masukan Jumlah Jeruk: "))
 qty_grape = int(input("Masukan Jumla Anggur: "))
-while qty_grape > stock_grape:
+while qty_grape > products[2][2]:
     print(f"Jumlah yang dimasukkan terlalu banyak !")
-    print(f"Stock Anggur Tinggal: {stock_grape}")
+    print(f"Stock Anggur Tinggal: {products[2][2]}")
     qty_grape = int(input("Masukan Jumlah Anggur: "))
 
 #Perhitungan
-total_apple = qty_apple*price_apple
-total_orange = qty_orange*price_orange
-total_grape = qty_grape*price_grape
+total_apple = qty_apple*products[0][1]
+total_orange = qty_orange*products[2][1]
+total_grape = qty_grape*products[3][1]
 grand_total = total_apple + total_orange + total_grape
 
 # Menu Detail Belanja
 print("\nDetail Belanja\n")
-print (f"Apel   : {qty_apple} x Rp.{price_apple:,} = Rp.{total_apple:,}")
-print (f"Jeruk  : {qty_orange} x Rp.{price_orange:,} = Rp.{total_orange:,}")
-print (f"Anggur : {qty_grape} x Rp.{price_grape:,} = Rp.{total_grape:,}")
+print (f"Apel   : {qty_apple} x Rp.{products[0][1]:,} = Rp.{total_apple:,}")
+print (f"Jeruk  : {qty_orange} x Rp.{products[1][1]:,} = Rp.{total_orange:,}")
+print (f"Anggur : {qty_grape} x Rp.{products[2][1]:,} = Rp.{total_grape:,}")
 print (f"\nTotal  : Rp.{grand_total:,}\n")
+
 
 #Payment Feature 
 print ("-"*10)
